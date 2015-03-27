4663962296a65c9819ef288e7e52bf74393f724b
Morten Siebuhr <sbhr@sbhr.dk>
2015-03-27 10:39:07 +0100

    Parser: Clean up time parsing.

--- parser.go
+++ parser.go
@@ -7,6 +7,15 @@ import (
 	"time"
 )
 
+var timeformats []string = []string{
+	// Output from Node.js' Winston logger
+	"2006-01-02T15:04:05.000000-07:00",
+
+	// RFC3339 / ISO.. timestamp
+	time.RFC3339Nano,
+	time.RFC3339,
+}
+
 func ParseLogEntries(in <-chan string, out chan<- LogLine) {
 	defer close(out)
 	for line := range in {
@@ -45,37 +54,26 @@ func ParseLogEntries(in <-chan string, out chan<- LogLine) {
 		}
 
 		// Usually, it's in the beginning
-		var lineTime time.Time
 		for i, part := range lineParts {
-			// 2015-03-06T08:10:13.326080+00:00
-			lineTime, err = time.Parse("2006-01-02T15:04:05.000000-07:00", part)
+			for _, timefmt := range timeformats {
+				lineTime, err := time.Parse(timefmt, part)
 
-			if err == nil {
-				newLine := make([]string, len(lineParts))
-				copy(newLine[:i], lineParts[:i])
-				copy(newLine[i:], lineParts[i+1:])
-				lineParts = newLine
-				break
-			}
-
-			// 2015-03-16T13:23:07.575Z
-			lineTime, err = time.Parse("2006-01-02T15:04:05.000Z", part)
-
-			if err == nil {
-				newLine := make([]string, len(lineParts))
-				copy(newLine[:i], lineParts[:i])
-				copy(newLine[i:], lineParts[i+1:])
-				lineParts = newLine
-				break
+				if err == nil {
+					logLine.Time = lineTime
+					newLine := make([]string, len(lineParts))
+					copy(newLine[:i], lineParts[:i])
+					copy(newLine[i:], lineParts[i+1:])
+					lineParts = newLine
+					break
+				}
 			}
 		}
 
-		if lineTime.IsZero() {
+		if logLine.Time.IsZero() {
 			fmt.Printf("Counld not find timestamp in line `%s`.\n", line)
 			continue
 		}
 
-		logLine.Time = lineTime
 		logLine.RawLine = []byte(strings.Join(lineParts, " "))
 		logLine.parseLogEntries()
 		out <- logLine
