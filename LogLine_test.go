a7b50797d5462c1d81a28b83488e8a6e7995c4de
Morten Siebuhr <sbhr@sbhr.dk>
2015-03-29 12:58:07 +0200

    LogLine: Test String() and MarshalJSON().

--- LogLine_test.go
+++ LogLine_test.go
@@ -3,6 +3,8 @@ package logmunch
 import (
 	"testing"
 	"time"
+
+	"encoding/json"
 )
 
 func TestLogLinesGetters(t *testing.T) {
@@ -49,5 +51,73 @@ func TestLogLinesGetters(t *testing.T) {
 	n = l.GetNumber("missing")
 	if n != 0 {
 		t.Errorf("Expected %+v to have missing=0, got %+v\n", l, n)
+	}
+}
+
+func TestLogLinesStringer(t *testing.T) {
+	l := NewLogLine(
+		time.Date(2015, 3, 29, 12, 29, 30, 5000000, time.UTC),
+		"some prefix",
+		map[string]string{
+			"a": "first",
+			"z": "last",
+		})
+
+	text := "2015-03-29T12:29:30.005Z some prefix a=first z=last"
+	out := l.String()
+
+	if out != text {
+		t.Errorf("LogLine.String()\nreturned `%s`\nexpected `%s`", text, out)
+	}
+}
+
+func TestLogLinesJSONEncoder(t *testing.T) {
+	l := NewLogLine(
+		time.Date(2015, 3, 29, 12, 29, 30, 5000000, time.UTC),
+		"some prefix",
+		map[string]string{
+			"key": "first",
+		},
+	)
+
+	// Encode to JSON
+	j, err := json.Marshal(l)
+	if err != nil {
+		t.Fatalf("Failed mashalling JSON: %s", err)
+	}
+
+	t.Logf("%v", string(j))
+
+	// Decode again
+	var out map[string]interface{}
+	err = json.Unmarshal(j, &out)
+	if err != nil {
+		t.Fatalf("Failed to unmarshal JSON: %s", err)
+	}
+
+	// Check date
+	if date, ok := out["time"]; !ok {
+		t.Errorf("JSON %s misses `time` field.", out)
+	} else if date.(string) != l.Time.Format(time.RFC3339Nano) {
+		t.Errorf("JSON %s .time returned `%s` field.", out, date)
+	}
+
+	// Check unix date
+	if date, ok := out["unixtime"]; !ok {
+		t.Errorf("JSON %s misses .unixtime.", out)
+	} else if date.(float64) != 1427632170005 {
+		t.Errorf("JSON %s .unixtime returned `%d`.", out, date)
+	}
+
+	// Check name
+	if name, ok := out["name"]; !ok {
+		t.Errorf("JSON %s misses `name` field.", out)
+	} else if name.(string) != "some prefix" {
+		t.Errorf("JSON %s .name returned `%s` field.", out, name)
+	}
+
+	// TODO Check key-values
+	if _, ok := out["entries"]; !ok {
+		t.Errorf("JSON %s misses .entries.", out)
 	}
 }
