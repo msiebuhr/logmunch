0fa86e5f520b0da915b7e755b3fd00b62d9b77ce
Morten Siebuhr <sbhr@sbhr.dk>
2015-03-30 09:37:40 +0200

    Query: gofmt.

--- Query.go
+++ Query.go
@@ -1,36 +1,36 @@
 package logmunch
 
 import (
-	"time"
 	"strings"
+	"time"
 )
 
 // QueryGroup
 type QueryGroup struct {
-    Name string
-    Keys []string
+	Name string
+	Keys []string
 }
 
 func NewQueryGroup(name string, keys []string) QueryGroup {
-    return QueryGroup{
-        Name: name,
-        Keys: keys,
-    }
+	return QueryGroup{
+		Name: name,
+		Keys: keys,
+	}
 }
 
 // Create a new key in l by name and composed of the given keys.
 func (q *QueryGroup) Group(l *LogLine) {
-    values := make([]string, len(q.Keys))
+	values := make([]string, len(q.Keys))
 
-    for i, key := range q.Keys {
-        if value, ok := l.Entries[key]; ok {
-            values[i] = value
-        } else {
-            values[i] = "-"
-        }
-    }
+	for i, key := range q.Keys {
+		if value, ok := l.Entries[key]; ok {
+			values[i] = value
+		} else {
+			values[i] = "-"
+		}
+	}
 
-    l.Entries[q.Name] = strings.Join(values, "×")
+	l.Entries[q.Name] = strings.Join(values, "×")
 }
 
 type Query struct {
@@ -48,5 +48,5 @@ type Query struct {
 	// TODO: Keep/discard some key/value pairs?
 	// TODO: Some display niceness?
 
-    GroupBy []QueryGroup
+	GroupBy []QueryGroup
 }
